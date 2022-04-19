c820c62071eb004b69bb8acfe006a00213677fcf
LenyN <lenyn@pop-os.localdomain>
2022-04-19 11:47:10 -0500

    secont commmit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola Mundo';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 7777777777;
 
 // Crea una variable booleana:
 const nuevoBool = null;
